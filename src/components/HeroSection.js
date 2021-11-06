d99fc4af5256693a1df3bfd25a592e073b961f70
Stackbit Code Editor <projects@stackbit.com>
2021-11-06 05:36:15 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -19,7 +19,7 @@ export default class HeroSection extends React.Component {
                     backgroundSize: backgroundSize,
                     backgroundRepeat: backgroundRepeat
             
-        <iframe src="https://drive.google.com/file/d/1AHiG0MN2B0XidAqVCTM-KrVvNsRaTtt6/preview" width="640" height="480" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1AHiG0MN2B0XidAqVCTM-KrVvNsRaTtt6/preview" width="640" height="480" allow="autoplay"></iframe>
 
                 }}
             />
